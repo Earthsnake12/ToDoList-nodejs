388497103a06a9a908ab1a6f3fb0b42ab1c5b2c8
Ignacio.Trenes <ignacio_m_16@hotmail.com>
2021-07-21 13:34:16 -0300

    actualizacion

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ const HTMLParser = require('node-html-parser');
 const app = express()
 
 app.get('/', function (req, res) {
+
   fs.readFile("./inicio/index.html", (err, data) => {
+    
     if (err) {
+
       res.writeHead(404);
       res.write('Whoops! File not found!');
       res.end();
-    } else {
-      let root = HTMLParser.parse(data);
-      //root.querySelector('#pp').replaceWith('<div>casa</div>');
 
+    } else {
+      
+      let root = HTMLParser.parse(data);
+      let tabla = require("./inicio/geradorTabla.js");
+      root.querySelector('#pp').replaceWith(tabla);
+      
       res.setHeader("Content-Type", "text/html");
       res.writeHead(200);
       res.end(root.toString());
@@ -23,8 +29,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(8000)
-/*
-var pendiente = require("./data.json");
-console.log(pendiente.Pendientes[0].descripcion);
-*/
+
 console.log('Servidor en la url http://127.0.0.1:8000/');
--- inicio/geradorTabla.js
+++ inicio/geradorTabla.js
@@ -1,3 +1,19 @@
-function genera_tabla() {
+function generarTablaGeneral() {
+
+  let pendientes = require("../data/General.json");
+  let tabla = "<table BORDER><tr><td>ID</td><td>DESCRIPCION</td><td>ESTADO</td><td>LINK</td></tr>"
+
+  for (let i = 0; i < pendientes.General.length; i++) {
     
+    tabla += "<tr>";
+    tabla += "<td>" + pendientes.General[i].id + "</td>";
+    tabla += "<td>" + pendientes.General[i].descripcion + "</td>";
+    tabla += "<td>" + pendientes.General[i].estado + "</td>";
+    tabla += "<td><button type='Button'>Click Me!</button></td>";
+    tabla += "</tr>";
   }
+  tabla += "</table>";
+  return tabla;
+}
+
+module.exports = generarTablaGeneral();
