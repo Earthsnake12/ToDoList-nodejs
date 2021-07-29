c48138823896c39429df5c2ccff7479c8ea95a33
Ignacio.Trenes <ignacio_m_16@hotmail.com>
2021-07-29 13:46:28 -0300

    Crea nueva tarea

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-
 const express = require('express');
 const fs = require("fs");
 const HTMLParser = require('node-html-parser');
+const bodyParser = require('body-parser');
+const { nextTick } = require('process');
 
-const app = express()
+const app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { //pagina de inicio
 
   fs.readFile("./inicio/index.html", (err, data) => {
-    
-    if (err) {
 
+    if (err) {
       res.writeHead(404);
       res.write('Whoops! File not found!');
       res.end();
 
     } else {
-      
       let root = HTMLParser.parse(data);
-      let tabla = require("./inicio/geradorTabla.js");
-      root.querySelector('#pp').replaceWith(tabla);
-      
+
+      try {
+        let tabla = require("./inicio/geradorTabla.js");
+        root.querySelector('#pp').replaceWith(tabla);
+      } catch (error) { }
+
       res.setHeader("Content-Type", "text/html");
       res.writeHead(200);
       res.end(root.toString());
@@ -30,7 +33,28 @@ app.get('/', function (req, res) {
   });
 })
 
-app.use(express.static(__dirname + '/public'));
+app.post('/tarea', function (req, res) {
+
+  let id;
+  try {
+    id = require("./data/General.json").UltimoId + 1; //
+  } catch (Error) {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(404);
+    res.end("Archivo general no encontrado");
+    next();
+  }
+
+  let fs = require('fs');
+  fs.writeFile("./data/pendientes/"+id+".json", JSON.stringify(req.body), function (err, result) {
+    if (err) console.log('error', err);
+  });
+
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  res.end("Nueva tarea cargada");
+});
+
 app.listen(8000)
 
 console.log('Servidor en la url http://127.0.0.1:8000/');
--- inicio/geradorTabla.js
+++ inicio/geradorTabla.js
@@ -1,13 +1,13 @@
 function generarTablaGeneral() {
 
   let pendientes = require("../data/General.json");
-  let tabla = "<table><tr><th>ID</th><th>DESCRIPCION</th><th>ESTADO</th><th>LINK</th></tr>"
+  let tabla = "<table><tr><th>ID</th><th>TITULO</th><th>ESTADO</th><th>LINK</th></tr>"
 
   for (let i = 0; i < pendientes.General.length; i++) {
     
     tabla += "<tr>";
     tabla += "<td>" + pendientes.General[i].id + "</td>";
-    tabla += "<td>" + pendientes.General[i].descripcion + "</td>";
+    tabla += "<td>" + pendientes.General[i].titulo + "</td>";
     tabla += "<td>" + pendientes.General[i].estado + "</td>";
     tabla += "<td><button type='Button'>Click Me!</button></td>";
     tabla += "</tr>";
