372f1ebd6c87ee80d92ed61be960a9fd7d1a61ca
Ignacio.Trenes <ignacio_m_16@hotmail.com>
2021-08-02 16:50:02 -0300

    Mejoras

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require("fs");
 const bodyParser = require('body-parser');
+
 const middlewares = require('./servicios/middleware'); //carga los middleware
 
 const app = express();
@@ -10,28 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', middlewares.paginaInicio);
 
-app.post('/tarea', function (req, res) {
-
-  let id;
-  try {
-    id = require("./data/General.json").UltimoId + 1; 
-  } catch (Error) {
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(404);
-    res.end("Archivo general no encontrado");
-    next();
-  }
-
-  console.log(JSON.stringify(req.body));
-  
-  fs.writeFile("./data/pendientes/" + id + ".json", JSON.stringify(req.body), function (err, result) {
-    if (err) console.log('error', err);
-  });
-
-  res.setHeader("Content-Type", "text/html");
-  res.writeHead(200);
-  res.end("Nueva tarea cargada");
-});
+app.post('/tarea', middlewares.cargarTarea);
 
 app.listen(8000)
 
--- servicios/middleware.js
+++ servicios/middleware.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
+
 const HTMLparser = require('node-html-parser');
 
 const middlewares = {
 
+    //carga la pag de inicio, revisa los pendientes y crea la tabla
     paginaInicio: function (req, res, next) {
 
         fs.readFile("./paginasHTML/index.html", (err, data) => {
@@ -26,6 +28,28 @@ const middlewares = {
                 res.end(root.toString());
             }
         });
+    },
+
+    //busca el id en el general, lo actualiza y carga una nueva tarea
+    cargarTarea: function (req, res, next) {
+
+        let id;
+        try {
+            id = require("../data/General.jso").UltimoId + 1;
+        } catch (error) {
+            console.log(error);
+            res.end("Archivo general no encontrado");
+        }
+
+        if (id !== undefined) {
+            console.log(JSON.stringify(req.body));
+            /*
+            fs.writeFile("./data/pendientes/" + id + ".json", JSON.stringify(req.body), function (err, result) {
+              if (err) console.log('error', err);
+            });*/
+
+            res.end("Nueva tarea cargada");
+        }
     }
 };
 module.exports = middlewares;
