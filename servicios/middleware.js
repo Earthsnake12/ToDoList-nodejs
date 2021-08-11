3635b918ddb40448f094259515184326cf6f9e6d
Ignacio.Trenes <ignacio_m_16@hotmail.com>
2021-08-11 11:05:41 -0300

    Cambio en la generacion de tabla

--- servicios/middleware.js
+++ servicios/middleware.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-
 const HTMLparser = require('node-html-parser');
 
 const middlewares = {
@@ -13,20 +12,50 @@ const middlewares = {
                 res.writeHead(404);
                 res.write('No se pudo cargar la pagina');
                 res.end();
-            } else {
+                return;
+            }
 
-                let root = HTMLparser.parse(data);
-                try {
-                    let tabla = require("./geradorTabla.js");
-                    root.querySelector('#lista').replaceWith(tabla);
+            let root = HTMLparser.parse(data); //Pagina base
+            let tabla = '<table id="lista" ><tr><th>ID</th><th>TITULO</th><th>ESTADO</th><th>LINK</th></tr>' //tabla a generar
 
-                } catch (error) {
-                    root.querySelector('#lista').replaceWith("<div>No se pudo cargar la tabla</div>");
+            fs.readFile("./data/General.json", 'utf8', (err, general) => {
+
+                if (err) {
+                    tabla = "<h1>No se pudo cargar la tabla</h1>";
+
+                } else {
+                    let pendientes = JSON.parse(general).Pendientes;
+
+                    pendientes.forEach(tarea => {
+                        tabla += "<tr>";
+                        tabla += "<td>" + tarea.id + "</td>";
+                        tabla += "<td>" + tarea.titulo + "</td>";
+                        tabla += "<td>" + tarea.estado + "</td>";
+                        tabla += "<td><button type='Button'>Click Me!</button></td>";
+                        tabla += "</tr>";
+
+                    });
+                    tabla += "</table>";
                 }
+
+                root.querySelector('#lista').replaceWith(tabla); //cargo la tabla en la pag
+
                 res.setHeader("Content-Type", "text/html");
                 res.writeHead(200);
                 res.end(root.toString());
-            }
+            });
+
+
+            /*
+            try {
+                let tabla = require("./geradorTabla.js");
+                root.querySelector('#lista').replaceWith(tabla);
+ 
+            } catch (error) {
+                root.querySelector('#lista').replaceWith("<div>No se pudo cargar la tabla</div>");
+            }*/
+
+
         });
     },
 
@@ -51,7 +80,7 @@ const middlewares = {
             req.body.avance = ["Tarea registrada"]; // registro avance
 
             generalData.Pendientes.push(req.body);
-            
+
             //guardo JSON actualizado
             fs.writeFile("./data/General.json", JSON.stringify(generalData), function (err, result) {
                 if (err) {
@@ -65,7 +94,7 @@ const middlewares = {
             //actualizo la tabla
 
             res.writeHead(200);
-            res.end("Tarea cargada"); //mando tabla actualizada
+            res.end("Tarea cargada");
 
             console.log("Nueva tarea: " + JSON.stringify(req.body));
         });
