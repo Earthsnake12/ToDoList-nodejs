509019795720f7b894272349f697db101e5a6ff1
5p4c3 <ignacio_m_16@hotmail.com>
2021-09-09 13:55:34 -0300

    carga archivo en caroeta y actualiza el general

--- rutas/upload.js
+++ rutas/upload.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const fs = require("fs");
 
 //maneja la subida de archivos
-router.post('/',(req,res) => {
-    
+router.post('/', (req, res) => {
+
     const id = parseInt(req.query.id, 10); //pasar el parametro como /upload?id=1
     let EDFile = req.files.file
-    
-    EDFile.mv("./data/files/" + id + "/" + EDFile.name, err => {
+
+    let date = new Date();
+    let fechaHora = date.getFullYear() + "-" + (1 + date.getMonth()) + "-" + date.getDate()
+        + "-" + date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
+
+    let ubicacion = "./data/files/" + id + "/" + fechaHora + path.extname(EDFile.name); //donde se guardara el archivo
+
+    EDFile.mv(ubicacion, err => {
         if (err) {
             console.log(err);
             res.writeHead(503);
@@ -15,9 +23,38 @@ router.post('/',(req,res) => {
             return;
         }
 
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
-        res.end("Archivo guardado");
+        fs.readFile("./data/General.json", 'utf8', (err, general) => {
+
+            if (err) {
+                res.setHeader("Content-Type", "text/html");
+                res.writeHead(503);
+                res.end("No se pudo cargar la base de datos");
+                return;
+
+            }
+            let archivoGeneral = JSON.parse(general);
+
+            //obtengo el indice de la tarea en el general
+            const tareaIndice = archivoGeneral.Pendientes.map(pendiente => pendiente.id).indexOf(id);
+            archivoGeneral.Pendientes[tareaIndice].files.push(ubicacion);
+
+
+            //actualizo el archivo general
+            fs.writeFile("./data/General.json", JSON.stringify(archivoGeneral), function (err, result) {
+
+                if (err) {
+                    console.log(err);
+                    res.writeHead(503);
+                    res.end("No se pudo actualizar Archivo General");
+                    return;
+                }
+
+                console.log("Archivo guardado en: " + ubicacion)
+                res.setHeader("Content-Type", "text/html");
+                res.writeHead(200);
+                res.end("Archivo guardado");
+            });
+        });
     });
 })
 
