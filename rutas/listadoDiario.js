45a3f9f3a5b6880ee399465fb4a1fe5db3e75252
Ignacio.Trenes <ignacio_m_16@hotmail.com>
2021-10-22 16:42:16 -0300

    Agregar nueva tarea en el registro diario

--- rutas/listadoDiario.js
+++ rutas/listadoDiario.js
@@ -7,7 +7,7 @@ const HTMLparser = require('node-html-parser');
 router.get('/', function (req, res) {
 
     let fecha = req.query.fecha  //pasar el parametro como ?fecha=1
-    console.log("Cargar listado del dia de" + fecha);
+    console.log("Cargar listado del dia de " + fecha);
 
     try {
         let pagina = fs.readFileSync("./paginasHTML/listadoDiario.html");
@@ -31,11 +31,11 @@ router.get('/', function (req, res) {
         var tabla = generarTabla(tareas, fecha);
 
         var fechas = "";
-        for (let i = 0; i < tareas.Fecha.length && i < 10; i++) {
+        for (let i = 0; i < tareas.fecha.length && i < 10; i++) {
 
             fechas += '<a href="/listadoDiario?fecha=';
-            fechas += tareas.Fecha[i] + '"style="margin: 0 5px;">'
-            fechas += tareas.Fecha[i] + '</a>';
+            fechas += tareas.fecha[i] + '"style="margin: 0 5px;">'
+            fechas += tareas.fecha[i] + '</a>';
         }
 
     } catch (err) {
@@ -44,7 +44,9 @@ router.get('/', function (req, res) {
     }
 
     root.querySelector('#lista').replaceWith(tabla); //cargo la tabla en la pag
-    root.querySelector('#linksFechas').replaceWith(fechas); //cargo la tabla en la pag
+    root.querySelector('#fecha').replaceWith("<p id='fecha'>" +fecha+ "</p>"); //cargo la fecha de la tabla en la pag
+
+    root.querySelector('#linksFechas').replaceWith(fechas); //cargo los link con las fechas en la pag
 
     res.setHeader("Content-Type", "text/html");
     res.writeHead(200);
@@ -52,9 +54,58 @@ router.get('/', function (req, res) {
 
 });
 
+router.post('/', function (req, res) {
+
+    let fecha = req.query.fecha  //pasar el parametro como ?fecha=1
+    console.log("Agrego tarea para el dia de " + fecha);
+
+    try {
+        let data = fs.readFileSync("./data/Diario.json", 'utf8');
+        var tareas = JSON.parse(data)
+
+    } catch (err) {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(503);
+        res.end("No se pudo cargar la base de datos");
+        return;
+    }
+
+    const indice = tareas.fecha.indexOf(fecha);
+
+    if (indice === -1) {
+        console.log("Error en la fecha pasada");
+
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(503);
+        res.end("Error en la fecha pasada");
+        return;
+    }
+
+    tareas.descripcion[indice].push(req.body.descripcion);
+    tareas.ids[indice].push("-");
+    tareas.estado[indice].push("Pendiente");
+
+    try {
+        fs.writeFileSync("./data/Diario.json", JSON.stringify(tareas));
+
+    } catch (err) {
+
+        console.log("Error al guardar el registro");
+
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(503);
+        res.end("Error al guardar el registro");
+        return;
+    }
+
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end();
+});
+
 function generarTabla(tareas, fecha) {
 
-    const fechaIndice = tareas.Fecha.indexOf(fecha);
+    const fechaIndice = tareas.fecha.indexOf(fecha);
 
     if (fechaIndice > -1) {
 
@@ -64,7 +115,7 @@ function generarTabla(tareas, fecha) {
         tabla += '<th>DESCRIPCION</th>';
         tabla += '</thead><tbody>'
 
-        let descripcion = tareas.Descripcion[fechaIndice];
+        let descripcion = tareas.descripcion[fechaIndice];
         let ids = tareas.ids[fechaIndice];
         let estado = tareas.estado[fechaIndice];
 
